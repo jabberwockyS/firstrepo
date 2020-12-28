3b9664eb9abcaecc67013df71baede5733df14e4
Sergio <jarasergio@outlook.com>
2020-12-27 21:17:12 -0300

    initial commit. including date-time.js first attepmt

---
+++ date-time.js
@@ -0,0 +1,16 @@
+let today = new Date();
+let day = today.getDay();
+const days = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+console.log("today is :" +days[day]);
+
+//let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+function timeAmPm(date){
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let ampm = hours >= 12 ? 'pm' : 'am';
+    hours= hours % 12;
+    let dateTime = hours + ampm + ':' + minutes + ':' + seconds
+    return dateTime
+}
+console.log(timeAmPm(new Date))
